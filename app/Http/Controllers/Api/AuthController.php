442564fea74f2daf640b85236e1d041b26ddac10
elhafkhalid-tech <elhaf.khalid@gmail.com>
2026-03-11 22:55:26 +0100

    instalation sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = user::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => hash::make($validated['password'])
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'utilisateur cree avec succes',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            return response()->json([
+                'message' => 'email or password invalid',
+            ]);
+        }
+
+        $user = user::where('email', $validated['email'])->first();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'connexion reussie',
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request){
+       $request->user()->currentAccessToken()->delete();
+
+       return response()->json([
+          'message' => "Deconnexion reussie",
+       ]);
+    }
+
+    public function user(Request $request){
+        return response()->json([
+            'user'=>$request->user(),
+        ]);
+    }
+}
+
